d0d1ea6ff5139a43cf8556808e0b978227fe0ee9
jeni63 <jeniferkue63@gmail.com>
2022-06-15 07:45:20 +0200

    first commit

--- pharmashop/models.py
+++ pharmashop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# liste des models
+#  -pharmacie
+#  -medicament
+#  -catégorie
+#  -utilisateur
+#  -maladie
+#  -symptôme
+#  -consultation
+#  -carnet (portant les informations de la consultation)
+
