b57a703a49b04f453f2bad0139215ee7a0947f8f
jeni63 <jeniferkue63@gmail.com>
2022-07-05 17:47:02 +0200

    recupération des mises à jours

--- pharmacy_app/settings.py
+++ pharmacy_app/settings.py
@@ -158,8 +158,8 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
 }
 
 
