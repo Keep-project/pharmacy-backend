4e55683d708ee4a89c3dec19db4c835c8f1db2bd
Patrick Kenne Tchinda <patrick1kenne@gmail.com>
2023-07-31 17:02:10 +0200

    Mise en place du module Faker pour enrégistrer quelques données de façon aléatoire dans la base de données

--- pharmashop/management/commands/createdata.py
+++ pharmashop/management/commands/createdata.py
@@ -1,6 +1,6 @@
 
 from faker import Faker
-
+from pharmashop import models
 from django.core.management.base import BaseCommand
 
 
@@ -17,7 +17,35 @@ class Command(BaseCommand):
         fake = Faker(["fr_FR"])
         
         
-        for _ in range(15):
-            print(fake.unique.address())
-            print(fake.unique.uuid4())
-            print("================================")
+        for i in range(15):
+            print("================ {0} ================".format(i))
+            # Créattion des utilisateurs
+            user = models.Utilisateur.objects.create_user(
+                username=fake.unique.first_name(),
+                password="patson120",
+                adresse=fake.unique.address(),
+                email=fake.unique.email(),
+                status= fake.unique.text(max_nb_chars=5),
+                experience= fake.random_int(0, 40),
+                avatar='',
+                is_active=True,
+                is_staff=True,
+                is_superuser=False
+            )
+            
+            # Créattion des Pharmacies
+            pharmacie = models.Pharmacie.objects.create(
+                libelle=fake.unique.text(max_nb_chars=10),
+                nom=fake.unique.company(),
+                localisation=fake.unique.address(),
+                tel = fake.unique.phone_number(),
+                email=fake.unique.email(),
+                latitude=fake.unique.latitude(),
+                longitude=fake.unique.longitude(),
+                h_ouverture = fake.unique.time(pattern = '%H:%M:%S'),
+                h_fermeture = fake.unique.time(pattern = '%H:%M:%S'),
+                user = user
+            )
+            
+        print(models.User.objects.all().count())
+        print(models.Pharmacie.objects.all().count())
